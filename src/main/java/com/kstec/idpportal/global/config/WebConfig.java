9e91ce852b24be756986c695950b6fce56bffcdf
Bindaedduck <seokjin.jang@kstec.co.kr>
2026-01-05 21:52:07 +0900

    [MOD] Global/config folder structure work.

--- src/main/java/com/kstec/idpportal/global/config/WebConfig.java
+++ src/main/java/com/kstec/idpportal/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.kstec.idpportal.global.config.config;
+package com.kstec.idpportal.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
